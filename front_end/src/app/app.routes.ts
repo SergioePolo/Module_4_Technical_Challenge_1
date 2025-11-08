02e51eea968f856a2e3687d16c1057e205f7c75e
Sergio Polo <129100808+DeveloperFundonal@users.noreply.github.com>
2025-11-08 11:39:12 -0500

    [create] Front end angular creation and routes configuration

---
+++ front_end/src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+import { Departments } from './pages/departments/departments';
+import { Roles } from './pages/roles/roles';
+import { User } from './pages/user/user';
+import { Welcome } from './pages/welcome/welcome';
+import { Admin } from './pages/admin/admin';
+import { NotFound } from './pages/not-found/not-found';
+
+export const routes: Routes = [
+    { path:'', component: Welcome, title: 'Bienvenida' },
+    { path:'roles', component: Roles, title: 'Gestión de roles' },
+    { path: 'departments', component: Departments, title: 'Gestión de departamentos' },
+    { path: 'employees', component: User, title: 'Gestión de empleados' },
+    { path: 'dashboard', component: Admin, title: 'Dashboard'},
+    { path: '**', component: NotFound, title: '404 - No encontrado' }
+];
---
+++ front_end/src/app/components/data-cards/data-cards.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-cards',
+  imports: [],
+  templateUrl: './data-cards.html',
+  styleUrl: './data-cards.css',
+})
+export class DataCards {
+
+}
